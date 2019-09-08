c9a8cff4aa3fa5507cc44c4f96596a7eecf3f75d
nobody0402 <haseo0402@gmail.com>
2019-09-08 23:13:02 +0800

    add practice 9

---
+++ python/9. Palindrome Number/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
+        if x < 0 :
+            return False
+        
+        y = 0
+        temp = x
+        while temp :
+            y = y * 10 + temp % 10
+            temp = temp // 10
+               
+        return x == y
+        
