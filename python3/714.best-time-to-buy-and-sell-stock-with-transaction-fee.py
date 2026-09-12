7e7050fbcb859f7f99bf93c9174b64c7975a936a
nobody <haseo0402@gmail.com>
2026-09-13 01:39:36 +0800

    update

---
+++ python3/714.best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=714 lang=python3
+#
+# [714] Best Time to Buy and Sell Stock with Transaction Fee
+#
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        
+# @lc code=end
+
